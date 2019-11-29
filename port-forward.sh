486db2d09897e6bfc54872b35f96393a756d7f88
Matheus Moraes <matheusfaria.moraes@gmail.com>
2019-11-29 16:21:54 -0300

    updating port-forward scripts

--- port-forward.sh
+++ port-forward.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-nohup kubectl port-forward svc/grafana -n istio-system 3000:3000 >/dev/null 2>&1 &
+set -e
+
+function sync {
+    echo "$(tput setaf 4)$1$(tput sgr0)"
+    eval $1
+}
+
+function async {
+    echo "$(tput setaf 4)$1$(tput sgr0)"
+    nohup $1 >/dev/null 2>&1 &
+}
+
+ps aux | grep '[k]ubectl.*port-forward.*svc/grafana.'| grep -v grep | awk '{print $2}' | xargs -r kill
+async "kubectl port-forward svc/grafana -n istio-system 3000:3000"
 echo "Forwarding Grafana - http://localhost:3000/"
-nohup kubectl port-forward svc/kiali -n istio-system 20001:20001 >/dev/null 2>&1 &
+
+ps aux | grep '[k]ubectl.*port-forward.*svc/kiali.'| grep -v grep | awk '{print $2}' | xargs -r kill
+async "kubectl port-forward svc/kiali -n istio-system 20001:20001"
 echo "Forwarding Kiali - http://localhost:20001/kiali/"
-nohup kubectl port-forward svc/tracing -n istio-system 8080:80 >/dev/null 2>&1 &
-echo "Forwarding Jaeger - http://localhost:8080/jaeger/search/"
+
+ps aux | grep '[k]ubectl.*port-forward.*svc/tracing.'| grep -v grep | awk '{print $2}' | xargs -r kill
+async "kubectl port-forward svc/tracing -n istio-system 9411:9411"
+echo "Forwarding Jaeger - http://localhost:9411/jaeger/search/"
+
+sync "ps aux | grep '[k]ubectl.'"
+
